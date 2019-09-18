1a930cd770249316ab1cfb63807b0798c5f1ca32
Joe1best <joebestavros@live.com>
2019-09-18 19:59:41 -0400

    Commented code because someone forced me too...

--- Shenanigans.py
+++ Shenanigans.py
@@ -4,10 +4,19 @@ import fbchat
 from getpass import getpass
 from fbchat.models import *
 
+#User gives their name
 username = str(input("Username: "))
+
+#Script asks for password
 client = fbchat.Client(username,getpass())
+
+#Name of friend who you want to send a message to 
 name = str(input("Name: ")) 
 friends = client.searchForUsers(name)  # return a list of names 
 friend = friends[0] 
+
+#Input message
 msg = str(input("Message: ")) 
+
+#Sends the message
 sent = client.send(Message(text = msg),thread_id=friend.uid)
