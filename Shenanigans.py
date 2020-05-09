5eb586ab229f0d6db2d1d05b271f83dd07228311
Joe1best <Joe1best@users.noreply.github.com>
2020-05-08 22:13:22 -0400

    Defined everything in functions. Made a send_meme function, it will display a big warning but it works for now

--- Shenanigans.py
+++ Shenanigans.py
@@ -4,19 +4,37 @@ import fbchat
 from getpass import getpass
 from fbchat.models import *
 
-#User gives their name
-username = str(input("Username: "))
+def login_fb():
+    global client
+    #User gives their name
+    username = str(input("Username: "))
+    
+    #Script asks for password
+    client = fbchat.Client(username,getpass())
 
-#Script asks for password
-client = fbchat.Client(username,getpass())
+def send_messsage():
+    
+    #Name of friend who you want to send a message to 
+    name = str(input("Name: ")) 
+    friends = client.searchForUsers(name)  # return a list of names 
+    friend = friends[0] 
+    
+    #Input message
+    msg = str(input("Message: ")) 
 
-#Name of friend who you want to send a message to 
-name = str(input("Name: ")) 
-friends = client.searchForUsers(name)  # return a list of names 
-friend = friends[0] 
+    #Sends the message
+    sent = client.send(Message(text = msg),thread_id=friend.uid)
 
-#Input message
-msg = str(input("Message: ")) 
+def send_meme(url):
+    #Name of friend who you want to send a message to 
+    name = str(input("Name: ")) 
+    friends = client.searchForUsers(name)  # return a list of names 
+    friend = friends[0] 
+    msg = str(input("What do you wanna say with your meme: "))
+    #Sends the message
+    sent = client.sendRemoteFiles(url,message=Message(text = msg),thread_id=friend.uid,)
 
-#Sends the message
-sent = client.send(Message(text = msg),thread_id=friend.uid)
+url = "https://i.pinimg.com/originals/44/9d/eb/449deb701dfebad299a39e40f4d404c0.jpg"
+login_fb()
+send_meme(url)
+client.logout()
